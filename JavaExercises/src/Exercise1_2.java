7b2efbac581455c17009ccb5aa9b33fc803e34bd
tnypls_mac <tnypls2786@gmail.com>
2020-04-15 14:32:26 +0900

    1-2,1-3

---
+++ JavaExercises/src/Exercise1_2.java
@@ -0,0 +1,13 @@
+
+public class Exercise1_2 {
+
+	public static void main(String[] args) {
+		// 練習問題1-2
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+
+		System.out.println("縦幅3横幅5の長方形の面積は、" + c);
+	}
+
+}
---
+++ JavaExercises/src/Exercise1_3.java
@@ -0,0 +1,11 @@
+
+public class Exercise1_3 {
+
+	public static void main(String[] args) {
+		boolean result = true;
+		char person = '駆';
+		double pi = 3.14;
+		long num = 314159265853979L;
+		String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+	}
+}
