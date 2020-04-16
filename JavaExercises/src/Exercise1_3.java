55e7bca3ae1164894ce370d76f24524839aa15b0
tnypls_mac <tnypls2786@gmail.com>
2020-04-16 10:10:55 +0900

    コメント追加

--- JavaExercises/src/Exercise1_3.java
+++ JavaExercises/src/Exercise1_3.java
@@ -3,10 +3,19 @@ public class Exercise1_3 {
 	// 練習問題1-3
 	// 型宣言をする問題
 	public static void main(String[] args) {
+		// ①ture
 		boolean result = true;
+
+		// ②'駆'
 		char person = '駆';
+
+		// ③3.14
 		double pi = 3.14;
+
+		// ④314159265853979L
 		long num = 314159265853979L;
+
+		// ⑤"ミナトの攻撃！敵に15ポイントのダメージを与えた。"
 		String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
 	}
 }
